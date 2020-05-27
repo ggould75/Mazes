9198d6953ded3201cafcf79901a928071c47308a
Marco Mussini <marco.mussini75@gmail.com>
2020-05-27 20:59:14 +0100

    Playing with UI Testing

--- Mazes/ViewController.swift
+++ Mazes/ViewController.swift
@@ -10,25 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var mazeTypeSegmentedControl: UISegmentedControl!
-    var mazeView: MazeView!
+    private var shouldIgnoreUserDefaults = false
+    var userDefaults = UserDefaults.standard
+
+    enum MazeType: Int, RawRepresentable, CaseIterable, Equatable {
+        case binaryTree
+        case sidewinder
+    }
+    var mazeType = MazeType.binaryTree
+
+    lazy var mazeTypeSegmentedControl: UISegmentedControl = {
+        let control = UISegmentedControl(items: ["Binary tree", "Sidewinder"])
+        control.accessibilityIdentifier = "MazeTypeSegmentedControlIdentifier"
+        control.translatesAutoresizingMaskIntoConstraints = false
+        control.selectedSegmentIndex = 0
+        let defaultAttributes = [ NSAttributedString.Key.foregroundColor: UIColor.red ]
+        control.setTitleTextAttributes(defaultAttributes, for: .normal)
+        control.addTarget(self, action: #selector(didChangeMazeType(_:)), for: .valueChanged)
+
+        return control
+    }()
+
+    lazy var mazeView: MazeView = {
+        let mazeView = MazeView(maze: randomMaze())
+        mazeView.translatesAutoresizingMaskIntoConstraints = false
+        mazeView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(didTapMaze)))
+
+        return mazeView
+    }()
+
+    init(shouldIgnoreUserDefaults: Bool = false) {
+        self.shouldIgnoreUserDefaults = shouldIgnoreUserDefaults
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        mazeTypeSegmentedControl = UISegmentedControl(items: ["Binary tree", "Sidewinder"])
-        mazeTypeSegmentedControl.translatesAutoresizingMaskIntoConstraints = false
-        mazeTypeSegmentedControl.selectedSegmentIndex = 0
-        let defaultAttributes = [ NSAttributedString.Key.foregroundColor: UIColor.red ]
-        mazeTypeSegmentedControl.setTitleTextAttributes(defaultAttributes, for: .normal)
-        mazeTypeSegmentedControl.addTarget(self, action: #selector(didChangeMazeType), for: .valueChanged)
         view.addSubview(mazeTypeSegmentedControl)
-
-        mazeView = MazeView(maze: randomMaze())
-        mazeView.translatesAutoresizingMaskIntoConstraints = false
-        mazeView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
         view.addSubview(mazeView)
-        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(didTap)))
 
         NSLayoutConstraint.activate([
             mazeTypeSegmentedControl.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
@@ -40,25 +65,43 @@ class ViewController: UIViewController {
             mazeView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             mazeView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
         ])
+
+        let initialMazeType = userDefaults.integer(forKey: "mazeType")
+        if !shouldIgnoreUserDefaults, initialMazeType >= 0, initialMazeType < MazeType.allCases.count {
+            mazeTypeSegmentedControl.selectedSegmentIndex = initialMazeType
+            mazeType = MazeType(rawValue: initialMazeType) ?? .binaryTree
+        } else {
+            userDefaults.set(0, forKey: "mazeType")
+            mazeType = MazeType(rawValue: 0) ?? .binaryTree
+        }
+        updateMazeView()
     }
 
-    func randomMaze() -> Grid {
-        let maze = Grid(rows: 60, columns: 30)
-        if mazeTypeSegmentedControl.selectedSegmentIndex == 0 {
+    @objc func didChangeMazeType(_ sender: UISegmentedControl) {
+        let segmentIndex = sender.selectedSegmentIndex
+        mazeType = MazeType(rawValue: segmentIndex) ?? .binaryTree
+        userDefaults.set(segmentIndex, forKey: "mazeType")
+        updateMazeView()
+    }
+
+    @objc func didTapMaze() {
+        updateMazeView()
+    }
+
+    private func randomMaze() -> Grid {
+        let maze = Grid(rows: 60, columns: 33)
+        switch mazeType {
+        case .binaryTree:
             maze.buildBinaryTreeMaze()
-        } else {
+        case .sidewinder:
             maze.buildSidewinderMaze()
         }
 
         return maze
     }
 
-    @objc func didChangeMazeType() {
-        mazeView?.maze = randomMaze()
-    }
-
-    @objc func didTap() {
-        mazeView?.maze = randomMaze()
+    private func updateMazeView() {
+        mazeView.maze = randomMaze()
     }
 }
 
